6da179080683e3ec9f3c9e7354b17e569e73b395
Denno Louis <54968137+dennolouis@users.noreply.github.com>
2022-12-18 01:37:04 -0500

    Vplat works

--- DroppyBall/Assets/Scripts/SpawnPlateform.cs
+++ DroppyBall/Assets/Scripts/SpawnPlateform.cs
@@ -6,10 +6,12 @@ public class SpawnPlateform : MonoBehaviour
 {
 
     [SerializeField] GameObject platform;
+    [SerializeField] GameObject verticlePlatform;
     // Start is called before the first frame update
     void Start()
     {
         Spawn();
+        SpawnVerticle();
     }
 
     // Update is called once per frame
@@ -24,5 +26,18 @@ public class SpawnPlateform : MonoBehaviour
         GameObject plat = Instantiate(platform, new Vector3(xpos, -5f, 0), Quaternion.identity);
         Destroy(plat, 4.5f);
         Invoke("Spawn", 0.5f);
+    }
+
+    void SpawnVerticle()
+    {
+        int n = Random.Range(1, 3);
+        for(int i = 0; i < n; i++)
+        {
+            float xpos = Random.Range(-2.5f, 2.5f);
+            GameObject vplat = Instantiate(verticlePlatform, new Vector3(xpos, -5f, 0), Quaternion.identity);
+            Destroy(vplat, 20f);
+        }
+        
+        Invoke("SpawnVerticle", Random.Range(20f, 40f));
     }
 }
